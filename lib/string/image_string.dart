b2c14c7c950985632bc06c9ad6c183b953e2d702
Anar95 <anarabbas458@gmail.com>
2023-05-07 01:10:59 +0300

    create

---
+++ lib/string/image_string.dart
@@ -0,0 +1,10 @@
+///________ splash_screen image  ______________________
+const String logo = "assets/logo.png";
+const String wave1 = "assets/wawe0.jpeg";
+
+///____ home page ________
+const String profile_1 = "assets/profile_1.png";
+const String imag_1 = "assets/imag_1.jpeg";
+const String imag_2 = "assets/imag_2.jpeg";
+const String imag_3 = "assets/imag_3.jpeg";
+const String imag_4 = "assets/imag_4.jpeg";
